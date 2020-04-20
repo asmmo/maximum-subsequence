cfc76a7586d7d52140b674d68a6fc4347fa93888
Ammar Sayed Mohammed Mohammed <ammarsayed99ph@gmail.com>
2020-04-21 00:11:47 +0200

    Update squareAlgo.cpp

--- squareAlgo.cpp
+++ squareAlgo.cpp
@@ -1,1 +1,18 @@
-
+#include <vector>
+template <typename T>
+T maxSubSum( const std::vector<T> & a )
+{
+    T maxSum {};
+    for( size_t i {}; i < a.size( ); ++i )
+        for( size_t j = i; j < a.size( ); ++j )
+        {
+            T thisSum {};
+            for( int k = i; k <= j; ++k )
+            {
+                thisSum += a[ k ];
+                if ( thisSum > maxSum )
+                    maxSum = thisSum;
+            }
+        }
+    return maxSum;
+}
