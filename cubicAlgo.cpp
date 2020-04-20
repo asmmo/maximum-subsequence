28f3e326dd7702cda9f3ecf725bf028a35b67dcb
Ammar Sayed Mohammed Mohammed <ammarsayed99ph@gmail.com>
2020-04-20 23:27:04 +0200

    Create cubicAlgo.cpp

---
+++ cubicAlgo.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+template <typename T>
+T maxSubSum( const std::vector<T> & a )
+{
+    T maxSum {};
+    for( size_t i {}; i < a.size( ); ++i )
+        for( size_t j = i; j < a.size( ); ++j )
+        {
+            T thisSum {};
+            for( int k = i; k <= j; ++k )
+                thisSum += a[ k ];
+            if ( thisSum > maxSum )
+                maxSum = thisSum;
+        }
+    return maxSum;
+}
